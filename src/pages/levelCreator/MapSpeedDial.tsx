3854fe97eb6a19e75c3733b3a8c63d68733a9452
SKairinos <skairinos@gmail.com>
2026-07-15 13:56:52 +0000

    quick save

--- src/pages/levelCreator/MapSpeedDial.tsx
+++ src/pages/levelCreator/MapSpeedDial.tsx
@@ -2,56 +2,77 @@ import {
   AddRoad as AddRoadIcon,
   Home as HomeIcon,
   RemoveRoad as RemoveRoadIcon,
-  type SvgIconComponent,
   Warehouse as WarehouseIcon,
 } from "@mui/icons-material"
 import { Box, SpeedDial, SpeedDialAction, SpeedDialIcon } from "@mui/material"
 import { type FC, useState } from "react"
 
-const actions: {
-  Icon: SvgIconComponent
-  title: string
-  backgroundColor: string
-}[] = [
-  { Icon: WarehouseIcon, title: "Mark Start", backgroundColor: "green" },
-  { Icon: HomeIcon, title: "Delete House", backgroundColor: "red" },
-  { Icon: HomeIcon, title: "Add House", backgroundColor: "green" },
-  { Icon: RemoveRoadIcon, title: "Delete Road", backgroundColor: "red" },
-  { Icon: AddRoadIcon, title: "Add Road", backgroundColor: "green" },
-]
+const actions = {
+  "mark-start": {
+    Icon: WarehouseIcon,
+    title: "Mark Start",
+    backgroundColor: "green",
+  },
+  "delete-house": {
+    Icon: HomeIcon,
+    title: "Delete House",
+    backgroundColor: "red",
+  },
+  "add-house": {
+    Icon: HomeIcon,
+    title: "Add House",
+    backgroundColor: "green",
+  },
+  "delete-road": {
+    Icon: RemoveRoadIcon,
+    title: "Delete Road",
+    backgroundColor: "red",
+  },
+  "add-road": {
+    Icon: AddRoadIcon,
+    title: "Add Road",
+    backgroundColor: "green",
+  },
+} as const
+type Action = keyof typeof actions
 
 export interface MapSpeedDialProps {}
 
 const MapSpeedDial: FC<MapSpeedDialProps> = () => {
+  const [action, setAction] = useState<Action>("add-road")
   const [open, setOpen] = useState(true)
-  const handleOpen = () => setOpen(true)
-  const handleClose = () => setOpen(false)
+
+  const ActionIcon = actions[action].Icon
 
   return (
-    <>
-      <Box sx={{ position: "fixed", right: 16, bottom: 16, zIndex: 1 }}>
-        <SpeedDial
-          ariaLabel="Create Map SpeedDial"
-          icon={<SpeedDialIcon />}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          open={open}
-        >
-          {actions.map(({ title, Icon, backgroundColor }) => (
+    <Box sx={{ position: "fixed", right: 16, bottom: 16, zIndex: 1 }}>
+      <SpeedDial
+        ariaLabel="Map SpeedDial"
+        icon={<SpeedDialIcon icon={<ActionIcon />} />}
+        onClose={(_, reason) => {
+          if (reason === "toggle") setOpen(false)
+        }}
+        onOpen={(_, reason) => {
+          if (reason === "toggle") setOpen(true)
+        }}
+        open={open}
+      >
+        {Object.entries(actions).map(
+          ([key, { title, Icon, backgroundColor }]) => (
             <SpeedDialAction
-              key={title}
+              key={key}
               icon={<Icon color="white" />}
               slotProps={{
                 fab: { sx: { backgroundColor } },
                 tooltip: { open: true, title },
                 staticTooltipLabel: { sx: { whiteSpace: "nowrap" } },
               }}
-              onClick={handleClose}
+              onClick={() => setAction(key as Action)}
             />
-          ))}
-        </SpeedDial>
-      </Box>
-    </>
+          ),
+        )}
+      </SpeedDial>
+    </Box>
   )
 }
 
