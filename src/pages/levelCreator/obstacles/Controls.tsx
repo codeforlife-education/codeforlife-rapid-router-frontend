f5f0b11283da24e0e50d8247e77e917944dd3f18
SKairinos <skairinos@gmail.com>
2026-08-19 13:44:55 +0000

    delete

--- src/pages/levelCreator/obstacles/Controls.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from "react"
-
-import ImageSelect, { type ImageSelectProps } from "./ImageSelect"
-
-export interface ControlsProps extends ImageSelectProps {}
-
-export const Controls: FC<ControlsProps> = props => <ImageSelect {...props} />
-
-export default Controls
