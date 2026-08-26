bb0a5d90556ee5d223d6b86616ee17707bf82d8b
Florian Aucomte <f.aucomte@hotmail.co.uk>
2026-08-26 13:26:54 +0200

    feat: Select character (#91)
    * Select code tab
    
    * Code controls modal
    
    * Remove unused import
    
    * Add description, subtitle and hint popup
    
    * Improvements
    
    * Character modal
    
    * Optimisations
    
    * Comment unused import
    
    * Improvements
    
    * Move steps counter
    
    * Make consistent with other modals
    
    * Improve state management and type safety
    
    ---------
    
    Co-authored-by: SKairinos <skairinos@gmail.com>

--- src/blockly/blocks/index.ts
+++ src/blockly/blocks/index.ts
@@ -4,11 +4,12 @@ import type * as defaults from "./defaults"
 import * as starts from "./starts"
 import { type BlockDefinition } from "../utils"
 
+export { booleans, commands, starts }
 export { type BooleanBlockType, BOOLEAN_BLOCK_TYPES } from "./booleans"
 export { type CommandBlockType, COMMAND_BLOCK_TYPES } from "./commands"
 export { type StartBlockType, START_BLOCK_TYPES } from "./starts"
 
-export const CUSTOM_BLOCKS = [
+export const DELETABLE_CUSTOM_BLOCKS = [
   // commands
   commands.MOVE_FORWARDS_BLOCK,
   commands.TURN_LEFT_BLOCK,
@@ -24,12 +25,17 @@ export const CUSTOM_BLOCKS = [
   booleans.AT_DESTINATION_BLOCK,
   booleans.COW_CROSSING_BLOCK,
   booleans.PIGEON_CROSSING_BLOCK,
+] as const satisfies BlockDefinition<string>[]
+
+export type DeletableBlockType =
+  | booleans.BooleanBlockType
+  | commands.CommandBlockType
+  | defaults.DefaultBlockType
+
+export const CUSTOM_BLOCKS = [
+  ...DELETABLE_CUSTOM_BLOCKS,
   // starts
   starts.VAN_BLOCK,
 ] as const satisfies BlockDefinition<string>[]
 
-export type BlockType =
-  | booleans.BooleanBlockType
-  | commands.CommandBlockType
-  | defaults.DefaultBlockType
-  | starts.StartBlockType
+export type BlockType = DeletableBlockType | starts.StartBlockType
